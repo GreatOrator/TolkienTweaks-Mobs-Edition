96f50e0fde4a1f5081fc1275cf60b009bc969858
GreatOrator <lord.technodragon@gmail.com>
2018-10-30 04:35:59 -0700

    Not sure why names are so hard part 2

--- src/main/java/com/greatorator/tolkienmobs/init/BlockInit.java
+++ src/main/java/com/greatorator/tolkienmobs/init/BlockInit.java
@@ -11,7 +11,7 @@ public class BlockInit {
     public static final List<Block> BLOCKS = new ArrayList<Block>();
 
     public static final Block BLOCK_MITHRIL = new BlockBase("block_mithril", Material.IRON);
-    public static final Block BLOCK_MITHRIL = new BlockBase("block_mithril", Material.IRON);
+    public static final Block BLOCK_MORGULIRON = new BlockBase("block_morguliron", Material.IRON);
     public static final Block ORE = new BlockOres("ore");
 
     public static final Block PLANKS = new BlockPlank("planks");
