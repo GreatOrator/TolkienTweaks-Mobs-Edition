4675a1469a940f65d015aa27189c69f6b5f5f38f
brandon3055 <brandon4261@gmail.com>
2019-02-21 14:56:27 +1100

    Fixed Armor Model

--- src/main/java/com/greatorator/tolkienmobs/init/ArmorInit.java
+++ src/main/java/com/greatorator/tolkienmobs/init/ArmorInit.java
@@ -8,6 +8,8 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ArmorInit extends ItemArmor implements TTMHasModel {
     public ArmorInit(ArmorMaterial materialIn, int renderIndexIn, EntityEquipmentSlot equipmentSlotIn) {
@@ -15,35 +17,46 @@ public class ArmorInit extends ItemArmor implements TTMHasModel {
     }
 
     @Override
-    public void registerModels()
-    {
+    public void registerModels() {
         TolkienMobs.proxy.registerModel(this, 0);
     }
 
+    @SideOnly(Side.CLIENT)
+    public ModelCustomArmor model;
+
+    @SideOnly(Side.CLIENT)
     @Override
-    public ModelBiped getArmorModel(EntityLivingBase entityLiving, ItemStack itemStack, EntityEquipmentSlot armorSlot, ModelBiped _default)
-    {
-        if(itemStack != ItemStack.EMPTY)
-        {
-            if(itemStack.getItem() instanceof ItemArmor)
-            {
-                ModelCustomArmor model = new ModelCustomArmor();
+    public ModelBiped getArmorModel(EntityLivingBase entityLiving, ItemStack itemStack, EntityEquipmentSlot armorSlot, ModelBiped _default) {
+        if (model == null) {
+            model = new ModelCustomArmor();
+            model.bipedHead.showModel = armorSlot == EntityEquipmentSlot.HEAD;
+            model.bipedBody.showModel = armorSlot == EntityEquipmentSlot.CHEST;
+            model.bipedRightLeg.showModel = armorSlot == EntityEquipmentSlot.FEET || armorSlot == EntityEquipmentSlot.LEGS;
+            model.bipedLeftLeg.showModel = armorSlot == EntityEquipmentSlot.FEET || armorSlot == EntityEquipmentSlot.LEGS;
+            model.bipedRightLeg.cubeList.clear();
+            model.bipedLeftLeg.cubeList.clear();
+            model.bipedHead.cubeList.clear();
+            model.bipedBody.cubeList.clear();
+            model.bipedLeftArm.cubeList.clear();
+            model.bipedRightArm.cubeList.clear();
 
-                model.bipedHead.showModel = armorSlot == EntityEquipmentSlot.HEAD;
-                model.bipedBody.showModel = armorSlot == EntityEquipmentSlot.CHEST;
-                model.bipedBody.showModel = armorSlot == EntityEquipmentSlot.LEGS;
-                model.bipedRightLeg.showModel = armorSlot == EntityEquipmentSlot.FEET;
-                model.bipedLeftLeg.showModel = armorSlot == EntityEquipmentSlot.FEET;
+            model.MithrilLF.showModel = armorSlot == EntityEquipmentSlot.FEET;
+            model.MithrilLFPart.showModel = armorSlot == EntityEquipmentSlot.FEET;
+            model.MithrilRF.showModel = armorSlot == EntityEquipmentSlot.FEET;
+            model.MithrilRFPart.showModel = armorSlot == EntityEquipmentSlot.FEET;
 
-                model.isChild = _default.isChild;
-                model.isRiding = _default.isRiding;
-                model.isSneak = _default.isSneak;
-                model.rightArmPose = _default.rightArmPose;
-                model.leftArmPose = _default.leftArmPose;
-
-                return model;
-            }
+            model.MithrilLL.showModel = armorSlot == EntityEquipmentSlot.LEGS;
+            model.MithrilLLPart.showModel = armorSlot == EntityEquipmentSlot.LEGS;
+            model.MithrilRL.showModel = armorSlot == EntityEquipmentSlot.LEGS;
+            model.MithrilRLPart.showModel = armorSlot == EntityEquipmentSlot.LEGS;
         }
-        return null;
+
+        model.isChild = _default.isChild;
+        model.isRiding = _default.isRiding;
+        model.isSneak = _default.isSneak;
+        model.rightArmPose = _default.rightArmPose;
+        model.leftArmPose = _default.leftArmPose;
+
+        return model;
     }
 }
