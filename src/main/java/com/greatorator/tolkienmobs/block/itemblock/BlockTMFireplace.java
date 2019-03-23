5eb42b4fbab0becf78a44947c0d2411cec1a5243
GreatOrator <lord.technodragon@gmail.com>
2019-03-22 19:46:29 -0700

    Starting work on new weapon features

--- src/main/java/com/greatorator/tolkienmobs/block/itemblock/BlockTMFireplace.java
+++ src/main/java/com/greatorator/tolkienmobs/block/itemblock/BlockTMFireplace.java
@@ -4,6 +4,9 @@ import com.brandon3055.brandonscore.blocks.BlockBCore;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockTMFireplace extends BlockBCore {
 //    public static final PropertyDirection FACING = BlockHorizontal.FACING;
@@ -118,15 +121,22 @@ public class BlockTMFireplace extends BlockBCore {
 //        return ((EnumFacing)state.getValue(FACING)).getIndex();
 //    }
 
+    @SideOnly(Side.CLIENT)
+    @Override
+    public BlockRenderLayer getBlockLayer()
+    {
+        return BlockRenderLayer.CUTOUT;
+    }
+
     @Override
     @Deprecated
     public boolean isOpaqueCube(IBlockState state) {
-        return true;
+        return false;
     }
 
     @Override
     @Deprecated
     public boolean isFullCube(IBlockState state) {
-        return true;
+        return false;
     }
 }
