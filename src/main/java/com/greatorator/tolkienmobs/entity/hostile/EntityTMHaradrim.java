d8c9e8130c7795233977b16328c2c90ccb7ac09c
GreatOrator <lord.technodragon@gmail.com>
2019-09-03 14:13:57 -0700

    World getting more robust!

--- src/main/java/com/greatorator/tolkienmobs/entity/hostile/EntityTMHaradrim.java
+++ src/main/java/com/greatorator/tolkienmobs/entity/hostile/EntityTMHaradrim.java
@@ -5,7 +5,6 @@ import com.greatorator.tolkienmobs.init.LootInit;
 import com.greatorator.tolkienmobs.init.SoundInit;
 import net.minecraft.block.Block;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.IRangedAttackMob;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
@@ -19,7 +18,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 
-public class EntityTMHaradrim extends EntityTMHostiles implements IRangedAttackMob {
+public class EntityTMHaradrim extends EntityTMHostiles {
     protected Block spawnableBlock = Blocks.SAND;
     private long nextAbilityUse = 0L;
     private final static long coolDown = 15000L;
@@ -30,7 +29,6 @@ public class EntityTMHaradrim extends EntityTMHostiles implements IRangedAttackM
         this.setSize(1.0F, 2.0F);
         this.setWeaponType(Items.IRON_SWORD);
         this.setLootTable(LootInit.BRIGAND);
-        this.setDualWield(true);
         this.setMob(this);
         this.setMobMentality(true, SoundInit.soundAngryGoblin);
         this.setRndMinMax(1,11);
