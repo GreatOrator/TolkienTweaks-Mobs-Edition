128974cd8a909e9e2860016a38d410f11772d732
GreatOrator <lord.technodragon@gmail.com>
2019-10-14 04:57:46 -0700

    Working out the bugs with tinkers integrations

--- src/main/java/com/greatorator/tolkienmobs/integration/TinkersTTM.java
+++ src/main/java/com/greatorator/tolkienmobs/integration/TinkersTTM.java
@@ -50,32 +50,12 @@ public class TinkersTTM {
         TinkerRegistry.registerMelting(TTMFeatures.BLOCK_MORGULIRON, FLUID_MORGULIRON, Material.VALUE_Block);
     }
 
-
-    public static void registerTcon() {
-//        mithril.setFluid(FLUID_MITHRIL);
-//        morguliron.setFluid(FLUID_MORGULIRON);
-//
-//        TinkerRegistry.integrate(mithril, FLUID_MITHRIL, "DwarvenSilver");
-//        TinkerRegistry.integrate(morguliron, FLUID_MORGULIRON, "MorgulIron");
-//
-//        TinkerSmeltery.registerToolpartMeltingCasting(mithril);
-//        TinkerSmeltery.registerToolpartMeltingCasting(morguliron);
-//
-//        TinkerRegistry.registerMelting(TTMFeatures.INGOT_MITHRIL, FLUID_MITHRIL, Material.VALUE_Ingot);
-//        TinkerRegistry.registerMelting(TTMFeatures.BLOCK_MITHRIL, FLUID_MITHRIL, Material.VALUE_Block);
-//
-//        TinkerRegistry.registerMelting(TTMFeatures.INGOT_MORGULIRON, FLUID_MORGULIRON, Material.VALUE_Ingot);
-//        TinkerRegistry.registerMelting(TTMFeatures.BLOCK_MORGULIRON, FLUID_MORGULIRON, Material.VALUE_Block);
-    }
-
     @Optional.Method(modid = "tconstruct")
     public static Material addMaterial(String name, String oredictSuffix, int color, Item.ToolMaterial material, ITrait trait) {
         Material mat = new Material(name, color);
         mat.setCraftable(false);
         mat.setCastable(false);
         mat.addCommonItems(oredictSuffix);
-//        mat.addItem("ingot" + oredictSuffix, 1, Material.VALUE_Ingot); //addCommonItems does this for you
-//        mat.addItem("block" + oredictSuffix, 1, Material.VALUE_Block);
         mat.addTrait(trait);
         mat.addStats(new HeadMaterialStats(material.getMaxUses() / 3 * 2, material.getEfficiency(), material.getAttackDamage() + 2, material.getHarvestLevel()));
         mat.addStats(new HandleMaterialStats(material.getEnchantability() / 15f, material.getMaxUses() / 3));
