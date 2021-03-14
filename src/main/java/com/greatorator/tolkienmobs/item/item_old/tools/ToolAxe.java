36220c38b7cc4ea02b2ae8834a48fa647328fcb8
GreatOrator <lord.technodragon@gmail.com>
2021-03-14 05:36:16 -0700

    Decided to work on more world gen features a bit

--- src/main/java/com/greatorator/tolkienmobs/item/item_old/tools/ToolAxe.java
+++
@@ -1,9 +0,0 @@
-//package com.greatorator.tolkienmobs.item.item_old.tools;
-//
-//import net.minecraft.item.ItemAxe;
-//
-//public class ToolAxe extends ItemAxe {
-//    public ToolAxe(ToolMaterial material, float damage, float speed) {
-//        super(material, damage, speed);
-//    }
-//}
