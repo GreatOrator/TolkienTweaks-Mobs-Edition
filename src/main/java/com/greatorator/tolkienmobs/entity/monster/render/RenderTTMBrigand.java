8d5d8882bcc6a3c639f9e28f4b6c558c0e4f6733
GreatOrator <lord.technodragon@gmail.com>
2021-10-24 09:17:28 -0700

    Back to work on Entities

--- src/main/java/com/greatorator/tolkienmobs/entity/monster/render/RenderTTMBrigand.java
+++ src/main/java/com/greatorator/tolkienmobs/entity/monster/render/RenderTTMBrigand.java
@@ -7,7 +7,7 @@ import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.util.ResourceLocation;
 
-public class RenderTTMBrigand  extends MobRenderer<EntityTTMBrigand, ModelTTMBrigand<EntityTTMBrigand>> {
+public class RenderTTMBrigand extends MobRenderer<EntityTTMBrigand, ModelTTMBrigand<EntityTTMBrigand>> {
     public RenderTTMBrigand(EntityRendererManager renderManagerIn) {
         super(renderManagerIn, new ModelTTMBrigand<>(0.0F, true), 1.0F);
     }
