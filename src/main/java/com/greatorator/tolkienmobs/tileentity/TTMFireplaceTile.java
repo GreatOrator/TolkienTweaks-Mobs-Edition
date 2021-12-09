e66dec8542e7b6ac02e1404f82fcc54a6929b7e1
GreatOrator <lord.technodragon@gmail.com>
2021-12-09 09:30:10 -0700

    Adding missing recipes and working on JEI integration

--- src/main/java/com/greatorator/tolkienmobs/tileentity/TTMFireplaceTile.java
+++ src/main/java/com/greatorator/tolkienmobs/tileentity/TTMFireplaceTile.java
@@ -1,7 +1,10 @@
 package com.greatorator.tolkienmobs.tileentity;
 
 import com.brandon3055.brandonscore.blocks.TileBCore;
-import com.brandon3055.brandonscore.inventory.*;
+import com.brandon3055.brandonscore.inventory.ContainerBCTile;
+import com.brandon3055.brandonscore.inventory.ContainerSlotLayout;
+import com.brandon3055.brandonscore.inventory.ItemHandlerIOControl;
+import com.brandon3055.brandonscore.inventory.TileItemStackHandler;
 import com.brandon3055.brandonscore.lib.IRSSwitchable;
 import com.brandon3055.brandonscore.lib.datamanager.ManagedBool;
 import com.brandon3055.brandonscore.lib.datamanager.ManagedInt;
@@ -12,23 +15,28 @@ import com.greatorator.tolkienmobs.TolkienMobs;
 import com.greatorator.tolkienmobs.block.BlockTTMFireplace;
 import com.greatorator.tolkienmobs.crafting.IFireplaceInventory;
 import com.greatorator.tolkienmobs.handler.interfaces.IFireplaceRecipe;
-import net.minecraft.block.Blocks;
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
+import net.minecraft.entity.item.ExperienceOrbEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.container.Container;
 import net.minecraft.inventory.container.INamedContainerProvider;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
+import net.minecraft.item.crafting.AbstractCookingRecipe;
+import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.tileentity.FurnaceTileEntity;
 import net.minecraft.tileentity.ITickableTileEntity;
-import net.minecraft.util.Direction;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.world.World;
 import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
 
 import javax.annotation.Nullable;
-
 import java.util.List;
 
 import static com.brandon3055.brandonscore.lib.datamanager.DataFlags.SAVE_BOTH_SYNC_CONTAINER;
@@ -37,6 +45,7 @@ import static net.minecraft.util.Direction.*;
 
 public class TTMFireplaceTile extends TileBCore implements ITickableTileEntity, INamedContainerProvider, IRSSwitchable, IFireplaceInventory {
     public static final ContainerSlotLayout.LayoutFactory<TTMFireplaceTile> SLOT_LAYOUT = (player, tile) -> new ContainerSlotLayout().playerMain(player).allTile(tile.itemHandler);
+    private final Object2IntOpenHashMap<ResourceLocation> recipesUsed = new Object2IntOpenHashMap<>();
 
     public final ManagedBool isBurning = register(new ManagedBool("is_burning", false, SAVE_BOTH_SYNC_TILE));
     /**
@@ -182,6 +191,40 @@ public class TTMFireplaceTile extends TileBCore implements ITickableTileEntity, 
                 itemHandler.setStackInSlot(2, stack);
             }
         }
+    }
+
+    public void awardUsedRecipesAndPopExperience(PlayerEntity player) {
+        List<IRecipe<?>> list = this.getRecipesToAwardAndPopExperience(player.level, player.position());
+        player.awardRecipes(list);
+        this.recipesUsed.clear();
+    }
+
+    public List<IRecipe<?>> getRecipesToAwardAndPopExperience(World world, Vector3d vector3d) {
+        List<IRecipe<?>> list = Lists.newArrayList();
+
+        for(Object2IntMap.Entry<ResourceLocation> entry : this.recipesUsed.object2IntEntrySet()) {
+            world.getRecipeManager().byKey(entry.getKey()).ifPresent((p_235642_4_) -> {
+                list.add(p_235642_4_);
+                createExperience(world, vector3d, entry.getIntValue(), ((AbstractCookingRecipe)p_235642_4_).getExperience());
+            });
+        }
+
+        return list;
+    }
+
+    private static void createExperience(World world, Vector3d vector3d, int amount, float exp) {
+        int i = MathHelper.floor((float)amount * exp);
+        float f = MathHelper.frac((float)amount * exp);
+        if (f != 0.0F && Math.random() < (double)f) {
+            ++i;
+        }
+
+        while(i > 0) {
+            int j = ExperienceOrbEntity.getExperienceValue(i);
+            i -= j;
+            world.addFreshEntity(new ExperienceOrbEntity(world, vector3d.x, vector3d.y, vector3d.z, j));
+        }
+
     }
 
     @Nullable
