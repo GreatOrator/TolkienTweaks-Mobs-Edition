949d6a70f35a481cf771b5b02a15f2186849e44c
GreatOrator <lord.technodragon@gmail.com>
2022-01-21 15:38:44 -0700

    Adding missing recipes and working on JEI integration

--- src/main/java/com/greatorator/tolkienmobs/entity/merchant/villager/VillagerTTMUtility.java
+++ src/main/java/com/greatorator/tolkienmobs/entity/merchant/villager/VillagerTTMUtility.java
@@ -7,21 +7,11 @@ import net.minecraft.entity.merchant.villager.VillagerProfession;
 import net.minecraft.item.Item;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.village.PointOfInterestType;
-import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 
 import javax.annotation.Nullable;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.Set;
 
 public class VillagerTTMUtility {
-    private static Method blockStatesInjector;
-
-    static
-    {
-        VillagerTTMUtility.blockStatesInjector = ObfuscationReflectionHelper.findMethod(PointOfInterestType.class, "registerBlockStates", PointOfInterestType.class);
-    }
 
     public static Set<BlockState> getAllStates(Block block)
     {
@@ -30,86 +20,16 @@ public class VillagerTTMUtility {
 
     public static void fixPOITypeBlockStates(PointOfInterestType poiType)
     {
-        try
-        {
-            VillagerTTMUtility.blockStatesInjector.invoke(null, poiType);
-        }
-        catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e)
-        {
-            e.printStackTrace();
-        }
+        PointOfInterestType.registerBlockStates(poiType);
     }
 
     public static PointOfInterestType pointOfInterestType(String p1, Set<BlockState> p2, int p3, int p4)
     {
-        try
-        {
-            //          Constructor<PointOfInterestType> c = (Constructor<PointOfInterestType>)PointOfInterestType.class.getDeclaredConstructors()[1];
-            Constructor<PointOfInterestType> c = PointOfInterestType.class.getDeclaredConstructor(String.class, Set.class, Integer.TYPE, Integer.TYPE);
-            c.setAccessible(true);
-            return c.newInstance(p1, p2, p3, p4);
-        }
-        catch (NoSuchMethodException e)
-        {
-            e.printStackTrace();
-        }
-        catch (SecurityException e)
-        {
-            e.printStackTrace();
-        }
-        catch (InstantiationException e)
-        {
-            e.printStackTrace();
-        }
-        catch (IllegalAccessException e)
-        {
-            e.printStackTrace();
-        }
-        catch (IllegalArgumentException e)
-        {
-            e.printStackTrace();
-        }
-        catch (InvocationTargetException e)
-        {
-            e.printStackTrace();
-        }
-
-        return null;
+        return new PointOfInterestType(p1, p2, p3, p4);
     }
 
     public static VillagerProfession villagerProfession(String p1, PointOfInterestType p2, ImmutableSet<Item> p3, ImmutableSet<Block> p4, @Nullable SoundEvent p5)
     {
-        try
-        {
-            Constructor<VillagerProfession> c = VillagerProfession.class.getDeclaredConstructor(String.class, PointOfInterestType.class, ImmutableSet.class, ImmutableSet.class, SoundEvent.class);
-            c.setAccessible(true);
-            return c.newInstance(p1, p2, p3, p4, p5);
-        }
-        catch (NoSuchMethodException e)
-        {
-            e.printStackTrace();
-        }
-        catch (SecurityException e)
-        {
-            e.printStackTrace();
-        }
-        catch (InstantiationException e)
-        {
-            e.printStackTrace();
-        }
-        catch (IllegalAccessException e)
-        {
-            e.printStackTrace();
-        }
-        catch (IllegalArgumentException e)
-        {
-            e.printStackTrace();
-        }
-        catch (InvocationTargetException e)
-        {
-            e.printStackTrace();
-        }
-
-        return null;
+        return new VillagerProfession(p1, p2, p3, p4, p5);
     }
 }
