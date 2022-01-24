3f836eee4bbc2f7c57a08634a54b86f2a03ec676
GreatOrator <lord.technodragon@gmail.com>
2022-01-24 16:41:01 -0700

    Attempting to create a functional piggy bank

--- src/main/java/com/greatorator/tolkienmobs/client/gui/GuiTTMPiggyBank.java
+++ src/main/java/com/greatorator/tolkienmobs/client/gui/GuiTTMPiggyBank.java
@@ -6,18 +6,17 @@ import com.brandon3055.brandonscore.client.gui.modulargui.GuiElementManager;
 import com.brandon3055.brandonscore.client.gui.modulargui.ModularGuiContainer;
 import com.brandon3055.brandonscore.client.gui.modulargui.templates.TBasicMachine;
 import com.brandon3055.brandonscore.inventory.ContainerBCTile;
-import com.greatorator.tolkienmobs.client.TTMSprites;
 import com.greatorator.tolkienmobs.entity.tile.TTMPiggyBankTile;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.util.text.ITextComponent;
 
-import static com.brandon3055.brandonscore.client.gui.GuiToolkit.GuiLayout.DEFAULT;
+import static com.brandon3055.brandonscore.client.gui.GuiToolkit.GuiLayout.WIDE_EXTRA_TALL;
 import static com.brandon3055.brandonscore.client.gui.GuiToolkit.LayoutPos.TOP_LEFT;
 import static com.brandon3055.brandonscore.inventory.ContainerSlotLayout.SlotType.TILE_INV;
 
 public class GuiTTMPiggyBank extends ModularGuiContainer<ContainerBCTile<TTMPiggyBankTile>> {
 
-    protected GuiToolkit<GuiTTMPiggyBank> toolkit = new GuiToolkit<>(this, DEFAULT).setTranslationPrefix("gui.tolkienmobs.piggybank");
+    protected GuiToolkit<GuiTTMPiggyBank> toolkit = new GuiToolkit<>(this, WIDE_EXTRA_TALL).setTranslationPrefix("gui.tolkienmobs.piggybank");
 
     private TTMPiggyBankTile tile;
 
@@ -30,7 +29,7 @@ public class GuiTTMPiggyBank extends ModularGuiContainer<ContainerBCTile<TTMPigg
     public void addElements(GuiElementManager manager) {
         TBasicMachine template = toolkit.loadTemplate(new TBasicMachine(this, tile));
 
-        GuiElement<?> inputSlots = toolkit.createSlots(template.background, 1, 1, 4, (x, y) -> container.getSlotLayout().getSlotData(TILE_INV, 0), TTMSprites.get("item_coin"));
-        toolkit.placeInside(inputSlots, template.background, TOP_LEFT, 39, 16);
+        GuiElement<?> inputSlots = toolkit.createSlots(template.background, 9, 7, 1, (x, y) -> container.getSlotLayout().getSlotData(TILE_INV, (x * 9) + y), null);
+        toolkit.placeInside(inputSlots, template.background, TOP_LEFT, 8, 16);
     }
 }
