d6fdc76096f1d149fa094ab5610f21327f23f0ac
GreatOrator <lord.technodragon@gmail.com>
2022-09-16 15:21:21 -0700

    Almost have the first gui done!

--- src/main/java/com/greatorator/tolkienmobs/client/gui/BronzeKeyAccessScreen.java
+++ src/main/java/com/greatorator/tolkienmobs/client/gui/BronzeKeyAccessScreen.java
@@ -1,10 +1,8 @@
 package com.greatorator.tolkienmobs.client.gui;
 
-import codechicken.lib.math.MathHelper;
 import com.brandon3055.brandonscore.BCConfig;
 import com.brandon3055.brandonscore.client.BCSprites;
 import com.brandon3055.brandonscore.client.gui.GuiToolkit;
-import com.brandon3055.brandonscore.client.gui.modulargui.GuiElement;
 import com.brandon3055.brandonscore.client.gui.modulargui.GuiElementManager;
 import com.brandon3055.brandonscore.client.gui.modulargui.ModularGuiScreen;
 import com.brandon3055.brandonscore.client.gui.modulargui.baseelements.GuiButton;
@@ -14,23 +12,14 @@ import com.brandon3055.brandonscore.client.gui.modulargui.guielements.GuiTextFie
 import com.brandon3055.brandonscore.client.gui.modulargui.guielements.GuiTexture;
 import com.brandon3055.brandonscore.client.gui.modulargui.lib.GuiAlign;
 import com.brandon3055.brandonscore.client.gui.modulargui.templates.TGuiBase;
-import com.greatorator.tolkienmobs.client.TTMSprites;
 import com.greatorator.tolkienmobs.handler.interfaces.IKeyAccessTile;
-import com.greatorator.tolkienmobs.network.TolkienNetwork;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.IGuiEventListener;
-import net.minecraft.client.renderer.IRenderTypeBuffer;
-import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.model.RenderMaterial;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextFormatting;
 
-import java.util.LinkedList;
-
 public class BronzeKeyAccessScreen extends ModularGuiScreen implements IGuiEventListener {
     protected GuiToolkit<BronzeKeyAccessScreen> toolkit = new GuiToolkit<>(this, 200, 78).setTranslationPrefix("screen.tolkienmobs.bronze_key");
-    private LinkedList<TargetElement> targetElements = new LinkedList();
     private GuiButton consumeKey;
     private GuiButton toggleMode;
     private GuiButton permanent;
@@ -69,10 +58,7 @@ public class BronzeKeyAccessScreen extends ModularGuiScreen implements IGuiEvent
                     .setBorderColourL(GuiToolkit.Palette.Slot::border3D)
                     .setSize(150, 12)
                     .setXPos(temp.background.xPos() + bgPad + 1)
-                    .setMaxYPos(temp.background.maxYPos() - bgPad - 33, false)
-                    .setHoverText(e -> new String[]{
-                            TextFormatting.DARK_AQUA + toolkit.i18n("instructions")
-                    });
+                    .setMaxYPos(temp.background.maxYPos() - bgPad - 33, false);
             temp.background.addChild(codeBG);
             GuiLabel keyTitle = codeBG.addChild(new GuiLabel().setAlignment(GuiAlign.LEFT).setShadowStateSupplier(() -> BCConfig.darkMode))
                     .setDisplaySupplier(() -> toolkit.i18n("keycode"))
@@ -87,166 +73,14 @@ public class BronzeKeyAccessScreen extends ModularGuiScreen implements IGuiEvent
                     .setYSize(8)
                     .setTextColGetter(GuiToolkit.Palette.Slot::text)
                     .setMaxXPos(codeBG.maxXPos() - 1, true);
+            GuiTextField keyCode = toolkit.createTextField(temp.background)
+                    .setFieldEnabled(true)
+                    .setHoverText(TextFormatting.DARK_AQUA + toolkit.i18n("instructions"))
+                    .setValidator(toolkit.catchyValidator(s -> s.equals("") || Long.parseLong(s) >= 0))
+                    .setPos(codeBG.xPos() + 2, codeBG.maxYPos() - 11)
+                    .setSize(145,10);
         }else {
 
-        }
-    }
-
-    private class TargetElement extends GuiElement<TargetElement> {
-        private int index;
-        private GuiTextField keyTitle;
-        private GuiButton lock;
-        private GuiButton delete;
-        private int clickTime = 99;
-        private boolean mousePressed = false;
-        private boolean dragging = false;
-
-        public TargetElement(int index) {
-            this.index = index;
-            setYSize(12);
-            setHoverText(e -> new String[]{
-                    TextFormatting.AQUA + getName(),
-                    TextFormatting.GRAY + toolkit.i18n("right_click_tp"),
-                    TextFormatting.GRAY + toolkit.i18n("double_click_name"),
-                    TextFormatting.GRAY + toolkit.i18n("must_unlock"),
-                    TextFormatting.GRAY + toolkit.i18n("drag_to_move")
-            });
-        }
-
-        private boolean hasTarget() {
-            return false;
-        }
-
-        private String getName() {
-            return "[error]";
-        }
-
-        private void setName(String name) {
-            TolkienNetwork.sendKeyAccessMessage(2, e -> e.writeVarInt(index).writeString(name));
-        }
-
-        @Override
-        public void addChildElements() {
-            keyTitle = toolkit.createTextField(this, false)
-                    .setChangeListener(this::setName)
-                    .setPos(xPos() + 1, yPos() + 2)
-                    .setYSize(8)
-                    .setXSize(145)
-                    .setCanFocus(false)
-                    .setReturnListener(() -> keyTitle.setFocused(false))
-                    .setEnabledCallback(this::hasTarget);
-        }
-
-        @Override
-        public boolean mouseClicked(double mouseX, double mouseY, int button) {
-            boolean ret = false;
-            if (!hasTarget() || button == 2) return false;
-            boolean mouseOver = keyTitle.isMouseOver(mouseX, mouseY);
-
-            ret = ret || super.mouseClicked(mouseX, mouseY, button);
-            mousePressed = mouseOver;
-
-            //Select
-            if (!ret && mouseOver && index != selectedIndex) {
-                TolkienNetwork.sendKeyAccessMessage(3, e -> e.writeVarInt(index));
-                return true;
-            }
-            return ret;
-        }
-
-        @Override
-        public boolean mouseDragged(double mouseX, double mouseY, int clickedMouseButton, double dragX, double dragY) {
-            if (mousePressed && !isMouseOver(mouseX, mouseY)) {
-                dragging = draggingTarget = true;
-            }
-            return super.mouseDragged(mouseX, mouseY, clickedMouseButton, dragX, dragY);
-        }
-
-        @Override
-        public boolean mouseReleased(double mouseX, double mouseY, int button) {
-            if (dragging) {
-                TargetElement hovered = getHovered((int)mouseX, (int)mouseY);
-                if (hovered != null) {
-                    int moveIndex = mouseY > hovered.yPos() + (hovered.ySize() / 2D) ? hovered.index + 1 : hovered.index;
-                    TolkienNetwork.sendKeyAccessMessage(4, e -> e.writeVarInt(moveIndex));
-                }
-            }
-
-            dragging = mousePressed = draggingTarget = false;
-            return super.mouseReleased(mouseX, mouseY, button);
-        }
-
-        @Override
-        public void renderElement(Minecraft minecraft, int mouseX, int mouseY, float partialTicks) {
-            boolean selected = false;
-            boolean hovered = false;
-
-            RenderMaterial mat = TTMSprites.get("keys/slot");
-            RenderMaterial matSelect = TTMSprites.get("keys/slot_selected");
-            IRenderTypeBuffer.Impl getter = IRenderTypeBuffer.immediate(Tessellator.getInstance().getBuilder());
-            drawSprite(getter.getBuffer(BCSprites.GUI_TYPE), xPos(), yPos(), (selected ? matSelect : mat).sprite());
-            if ((hovered && !selected)) {
-                getter.endBatch();
-                drawSprite(getter.getBuffer(BCSprites.GUI_TYPE), xPos(), yPos(), matSelect.sprite(), 0x30FFFFFF);
-            }
-            getter.endBatch();
-            super.renderElement(minecraft, mouseX, mouseY, partialTicks);
-
-            if (lock.isMouseOver(mouseX, mouseY) || (delete.isEnabled() && delete.isMouseOver(mouseX, mouseY))) {
-                hoverTime = 0;
-            }
-        }
-
-        @Override
-        public boolean renderOverlayLayer(Minecraft minecraft, int mouseX, int mouseY, float partialTicks) {
-            if (dragging) {
-                int yPos = MathHelper.clip(mouseY - 6, getParent().yPos(), getParent().maxYPos() - 12);
-                IRenderTypeBuffer.Impl getter = IRenderTypeBuffer.immediate(Tessellator.getInstance().getBuilder());
-
-                TargetElement hovered = getHovered(mouseX, mouseY);
-                if (hovered != null) {
-                    int y = mouseY > hovered.yPos() + (hovered.ySize() / 2) ? hovered.maxYPos() : hovered.yPos() - 1;
-                    drawColouredRect(getter, xPos(), y, xSize(), 3, 0x6000FF00);
-                    getter.endBatch();
-                }
-
-                drawSprite(getter.getBuffer(BCSprites.GUI_TYPE), xPos(), yPos, TTMSprites.get("keys/slot_selected").sprite(), 0x60FFFFFF);
-                getter.endBatch();
-
-
-                return true;
-            } else if (draggingTarget) {
-                return false;
-            }
-
-            return super.renderOverlayLayer(minecraft, mouseX, mouseY, partialTicks);
-        }
-
-        private TargetElement getHovered(int mouseX, int mouseY) {
-            for (TargetElement element : targetElements) {
-                if (element != this && element.isMouseOver(mouseX, mouseY) || element.isMouseOver(mouseX, mouseY + 1) || element.isMouseOver(mouseX, mouseY - 1)) {
-                    return element;
-                }
-            }
-            return null;
-        }
-
-        @Override
-        public boolean onUpdate() {
-            if (lastAdded == index && editAdded) {
-                lastAdded = -1;
-                editAdded = false;
-                keyTitle.setFocused(true);
-                keyTitle.setCursorPositionEnd();
-                keyTitle.setSelectionPos(0);
-            }
-
-            return super.onUpdate();
-        }
-
-        @Override
-        public boolean isHoverTextEnabled() {
-            return hasTarget();
         }
     }
 }
