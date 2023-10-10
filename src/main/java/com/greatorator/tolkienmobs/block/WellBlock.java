be5b4d65b7baee2ea24dffa7bb18ab31bb7e8ef8
GreatOrator <GreatOrator@users.noreply.github.com>
2023-10-10 11:19:06 -0700

    WorldGen continues...

--- src/main/java/com/greatorator/tolkienmobs/block/WellBlock.java
+++ src/main/java/com/greatorator/tolkienmobs/block/WellBlock.java
@@ -4,6 +4,17 @@ import com.brandon3055.brandonscore.blocks.BlockBCore;
 import com.greatorator.tolkienmobs.init.TolkienTiles;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.alchemy.PotionUtils;
+import net.minecraft.world.item.alchemy.Potions;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
@@ -21,6 +32,7 @@ import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
@@ -99,5 +111,72 @@ public class WellBlock extends BlockBCore implements EntityBlock {
     @Override
     public FluidState getFluidState(BlockState state) {
         return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
+    }
+
+    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
+        ItemStack heldItem = player.getItemInHand(hand);
+        ItemStack filledBottle = PotionUtils.setPotion(new ItemStack(Items.POTION), Potions.WATER);
+
+        if (heldItem.getItem() == Items.BUCKET) {
+            CompoundTag tagCompound = heldItem.getTag();
+            ItemStack newItem = Items.WATER_BUCKET.getDefaultInstance();
+            if (tagCompound != null) {
+                newItem.setTag(tagCompound);
+            }
+            if (heldItem.getCount() <= 1) {
+                player.setItemInHand(hand, newItem);
+            } else {
+                if (player.getInventory().add(newItem)) {
+                    heldItem.shrink(1);
+                }
+            }
+            spawnParticlesAndPlaySound(level, pos, state);
+            level.playSound(null, pos, SoundEvents.BUCKET_EMPTY, SoundSource.BLOCKS, 1f, level.random.nextFloat() + 0.5f);
+            return InteractionResult.SUCCESS;
+        }else if (heldItem.getItem() == Items.GLASS_BOTTLE) {
+            CompoundTag tagCompound = heldItem.getTag();
+            if (tagCompound != null) {
+                filledBottle.setTag(tagCompound);
+            }
+            if (heldItem.getCount() <= 1) {
+                player.setItemInHand(hand, filledBottle);
+            } else {
+                if (player.getInventory().add(filledBottle)) {
+                    heldItem.shrink(1);
+                }
+            }
+            spawnParticlesAndPlaySound(level, pos, state);
+            level.playSound(null, pos, SoundEvents.BOTTLE_FILL, SoundSource.BLOCKS, 1f, level.random.nextFloat() + 0.5f);
+            return InteractionResult.SUCCESS;
+        }
+
+        return InteractionResult.SUCCESS;
+    }
+
+    private void spawnParticlesAndPlaySound(Level level, BlockPos pos, BlockState state) {
+        float dripWaterX = 0f;
+        float dripWaterZ = 0f;
+        switch (state.getValue(FACING)) {
+            case NORTH -> {
+                dripWaterZ = 0.25f;
+                dripWaterX = -0.05f;
+            }
+            case SOUTH -> dripWaterX = 0.25f;
+            case WEST -> {
+                dripWaterX = 0.25f;
+                dripWaterZ = 0.25f;
+            }
+            case EAST -> dripWaterZ = -0.05f;
+        }
+
+        float particleX = (float) pos.getX() + 0.5f;
+        float particleY = (float) pos.getY() + 1.25f;
+        float particleZ = (float) pos.getZ() + 0.5f;
+        level.addParticle(ParticleTypes.SPLASH, (double) particleX + dripWaterX, (double) particleY - 0.45f, (double) particleZ + dripWaterZ, 0, 0, 0);
+        for (int i = 0; i < 5; i++) {
+            level.addParticle(ParticleTypes.SPLASH, (double) particleX + Math.random() - 0.5f, (double) particleY + Math.random() - 0.5f, (double) particleZ + Math.random() - 0.5f, 0, 0, 0);
+        }
+
+        level.playSound(null, pos, SoundEvents.WATER_AMBIENT, SoundSource.BLOCKS, 0.1f, level.random.nextFloat() + 0.5f);
     }
 }
